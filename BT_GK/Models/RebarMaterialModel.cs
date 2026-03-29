976db4b24b45607dd1f12ecd7117d9023db45274
luancutee2k5 <ntluan231005@gmail.com>
2026-03-29 21:20:28 +0700

    Xong BT GK

---
+++ BT_GK/Models/RebarMaterialModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_GK.Models
+{
+    public class RebarMaterialModel
+    {
+        public string Name { get; set; }
+
+        public double Rs { get; set; }
+        public double Rsc { get; set; }
+    }
+}
+
---
+++ BT_GK/Views/BeamView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BT_GK.Views
+{
+    /// <summary>
+    /// Interaction logic for BeamView.xaml
+    /// </summary>
+    public partial class BeamView : Window
+    {
+        public BeamView()
+        {
+            InitializeComponent();
+        }
+        private void btnTinhToanDam_Click(object sender, RoutedEventArgs e)
+        {
+            dtg_BeamList.Items.Refresh();
+        }
+    }
+}
